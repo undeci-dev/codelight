40785db62a877431c8ac00df4e09757337403f17
mjlee <undeci0206@knou.ac.kr>
2025-10-17 17:22:26 +0900

    chore: 스프링 부트 세팅

---
+++ backend/codelight/src/main/java/com/project/codelight/CodelightApplication.java
@@ -0,0 +1,13 @@
+package com.project.codelight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelightApplication.class, args);
+	}
+
+}
