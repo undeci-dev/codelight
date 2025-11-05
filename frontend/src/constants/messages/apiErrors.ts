cad9c66e304104a9969729761c0f4f1c710978fd
mjlee <undeci0206@knou.ac.kr>
2025-11-05 14:01:30 +0900

    feat: 라우트 경로 및 메시지 상수 정의

---
+++ frontend/src/constants/messages/apiErrors.ts
@@ -0,0 +1,14 @@
+export const API_ERRORS = {
+  AUTH_ACCESS_TOKEN_EMPTY:
+    '로그인 후 이용 가능합니다.\n 로그인을 진행해 주세요.',
+  AUTH_TOKEN_EMPTY: '로그인 후 이용 가능합니다.\n 로그인을 진행해 주세요.',
+  AUTH_TOKEN_INVALID: '로그인 후 이용 가능합니다.\n 로그인을 진행해 주세요.',
+
+  USER_NOT_FOUND: '사용자를 찾을 수 없어요.\n 다시 확인해 주세요.',
+  USER_INVALID_FORMAT: '이메일 또는 비밀번호 형식이 올바르지 않아요.',
+  USER_ACCOUNT_LOCKED:
+    '여러 번 실패하여 계정이 잠겼어요. \n잠시 후 다시 시도해 주세요.',
+
+  NETWORK_UNAVAILABLE:
+    '네트워크에 연결할 수 없어요.\n 잠시 후 다시 시도해 주세요.',
+};
---
+++ frontend/src/constants/routhPath.ts
@@ -0,0 +1,6 @@
+export const ROUTE_PATH = {
+  root: '/',
+  signUp: '/sign-up',
+  signIn: '/sign-in',
+  home: '/sign-up',
+};
