984c88d4b6995a0297238a153a004fd9e06c043b
mjlee <undeci0206@knou.ac.kr>
2025-12-05 12:38:49 +0900

    refactor: S3Config에서 CloudFront 관련 설정 분리

--- backend/codelight/src/main/java/com/project/codelight/global/config/S3Config.java
+++ backend/codelight/src/main/java/com/project/codelight/global/config/S3Config.java
@@ -7,7 +7,6 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
@@ -28,14 +27,5 @@ public class S3Config {
                        .region(Region.of(region))
                        .credentialsProvider(StaticCredentialsProvider.create(credentials))
                        .build();
-    }
-
-    @Bean
-    public S3Presigner s3Presigner() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-        return S3Presigner.builder()
-                          .region(Region.of(region))
-                          .credentialsProvider(StaticCredentialsProvider.create(credentials))
-                          .build();
     }
 }
