1efa5662c115d7f74f2d396b7dd32899dd92665b
mjlee <undeci0206@knou.ac.kr>
2025-12-05 13:14:50 +0900

    feat: PostFileRepository에 조회 및 삭제 메서드 추가

--- backend/codelight/src/main/java/com/project/codelight/post/repository/PostFileRepository.java
+++ backend/codelight/src/main/java/com/project/codelight/post/repository/PostFileRepository.java
@@ -2,9 +2,16 @@ package com.project.codelight.post.repository;
 
 import com.project.codelight.post.domain.Post;
 import com.project.codelight.post.domain.PostFile;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostFileRepository extends JpaRepository<PostFile, Long> {
 
+    List<PostFile> findAllByPost(Post post);
+
+    List<PostFile> findAllByIdInAndPost(List<Long> ids, Post post);
+
     void deleteAllByPost(Post post);
+
+    void deleteAllByIdInAndPost(List<Long> ids, Post post);
 }
