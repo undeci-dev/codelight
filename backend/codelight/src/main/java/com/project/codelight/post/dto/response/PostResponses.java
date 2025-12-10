a100d9996ab3e78f7cac258d51fee93201c3abfb
mjlee <undeci0206@knou.ac.kr>
2025-12-10 17:40:29 +0900

    feat: 게시글 키워드 검색 기능 추가

--- backend/codelight/src/main/java/com/project/codelight/post/dto/response/PostResponses.java
+++ backend/codelight/src/main/java/com/project/codelight/post/dto/response/PostResponses.java
@@ -3,7 +3,8 @@ package com.project.codelight.post.dto.response;
 import java.util.List;
 
 public record PostResponses(
-    List<PostSummaryResponse> posts
+    List<PostSummaryResponse> posts,
+    boolean hasNext
 ) {
 
 }
