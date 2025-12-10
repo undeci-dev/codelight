6506573d790c18d819e149faa2507c89d89d7e11
undeci-dev <undeci0206@knou.ac.kr>
2025-12-11 02:17:17 +0900

    refactor: 라우터 설정 및 로그인 후 리다이렉트 경로 변경

--- frontend/src/routers/router.tsx
+++ frontend/src/routers/router.tsx
@@ -1,9 +1,11 @@
 import React, { Suspense } from 'react';
-import { createBrowserRouter, Outlet } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { ROUTE_PATH } from '@/constants/routhPath';
 import Layout from '@/components/_common/Layout/Layout';
+import GuestRoute from '@/components/_common/Route/GuestRoute';
 
 const MainPage = React.lazy(() => import('@/pages/MainPage'));
+const FeedPage = React.lazy(() => import('@/pages/FeedPage'));
 const SignUpPage = React.lazy(() => import('@/pages/SignUpPage'));
 const SignInPage = React.lazy(() => import('@/pages/SignInPage'));
 const KakaoCallbackPage = React.lazy(() => import('@/pages/KakaoCallbackPage'));
@@ -17,13 +19,19 @@ const router = createBrowserRouter([
       </Suspense>
     ),
     children: [
+      // 비로그인 사용자만 접근 가능
       {
-        element: <SignUpPage />,
-        path: ROUTE_PATH.signUp,
-      },
-      {
-        element: <SignInPage />,
-        path: ROUTE_PATH.signIn,
+        element: <GuestRoute />,
+        children: [
+          {
+            element: <SignUpPage />,
+            path: ROUTE_PATH.signUp,
+          },
+          {
+            element: <SignInPage />,
+            path: ROUTE_PATH.signIn,
+          },
+        ],
       },
       {
         element: <KakaoCallbackPage />,
@@ -32,6 +40,10 @@ const router = createBrowserRouter([
       {
         element: <MainPage />,
         path: ROUTE_PATH.main,
+      },
+      {
+        element: <FeedPage />,
+        path: ROUTE_PATH.feed,
       },
       {
         element: <PostDetailPage />,
